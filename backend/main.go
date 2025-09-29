cc35024f3e0405c6cfa304c46bb9e711d4e4935f
Vlad <osadchiy900@icloud.com>
2025-09-29 22:51:31 +0300

    Initial Commit Of Backend

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "up",
+		})
+	})
+
+	router.Run()
+}
